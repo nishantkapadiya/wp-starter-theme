c5bb70e9b09a798365128b1a4b95e54aa0a4afa9
Kanaksinh Rahevar <50567731+krzwt@users.noreply.github.com>
2022-10-20 17:15:38 +0530

    Update components.php

--- template-part/components.php
+++ template-part/components.php
@@ -1,12 +1,13 @@
 <?php
-if( have_rows('page_component')) {
+
+if( have_rows('page_component') ) {
 	$loop = 1;
     while ( have_rows('page_component') ) {
         the_row();
         $layout = get_row_layout();
         switch ( $layout ) {
             case $layout:
-                include(locate_template('template-part/components/_'.$layout.'.php'));
+                include(locate_template('template-part/components/'.$layout.'.php'));
             break;
         }
     }
