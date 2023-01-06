fbd7417d266107322f288a2443b9cd2f221b8448
Kanaksinh Rahevar <50567731+krzwt@users.noreply.github.com>
2023-01-06 11:31:06 +0530

    Update theme-scripts.php

--- includes/theme-scripts.php
+++ includes/theme-scripts.php
@@ -22,7 +22,11 @@ function action__front_script() {
     // Register and enqueue scripts js
     // wp_enqueue_script( THEME_PREFIX . '-jquery-js', 'https://code.jquery.com/jquery-3.6.3.min.js', array('jquery'), '3.6.3', true );
     // wp_enqueue_script( THEME_PREFIX . '-fancyapps-js', 'https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.umd.js', array('jquery'), '4.0.31', true );
-    wp_register_script( THEME_PREFIX . '-scripts-jquery', get_template_directory_uri() . '/assets/js/scripts.min.js', array( 'jquery' ), '1.0', true );
+    if (file_exists(get_stylesheet_directory() . '/assets/js/scripts.js')){
+        wp_register_script( THEME_PREFIX . '-scripts-jquery', get_template_directory_uri() . '/assets/js/scripts.js', array( 'jquery' ), '1.0', true );
+    }else{
+        wp_register_script( THEME_PREFIX . '-scripts-jquery', get_template_directory_uri() . '/assets/js/scripts.min.js', array( 'jquery' ), '1.0', true );
+    }
     wp_enqueue_script( THEME_PREFIX . '-scripts-jquery' );
 }
 add_action( 'wp_enqueue_scripts', 'action__front_script' );
